da65e76201d174ffab33ce1a4913d8fb84fc5e9d
deny <deny@google.com>
2023-05-09 22:03:11 +0700

    .

--- android/app/src/main/kotlin/com/example/hyper_ui/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hyper_ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hyper_ui
+package com.example.latihan_ui
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hyper_ui/core.dart';
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/debug.dart';
+
+import 'firebase_options.dart';
 
 /*
 Join Kelas Online /w DenyOcr
@@ -8,7 +11,13 @@ Dapatkan harga Flash Sale hari ini! Buka:
 ? capekngoding.com
 */
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await initialize();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   Get.mainTheme.value = getDarkTheme();
   runMainApp();
 }
